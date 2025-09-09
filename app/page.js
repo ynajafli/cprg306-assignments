eb1161f231fd04ba58ba68e6bfb5f12a3ca0e674
Yusif Najafli <ynajafli@outlook.com>
2025-09-09 14:17:46 -0600

    Updated page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>CPRG306 Assignments</h1>
+      <h1>CPRG306: Web Development 2 - Assignments</h1>
       <ul>
         <li>
           <Link href="./week-2/">Week 2 Assignment</Link>
