2b0b95fda6104f3d6b839e82dc4f85e08b2fc9b6
Yusif Najafli <ynajafli@outlook.com>
2025-09-09 14:15:12 -0600

    Finished assignment 2

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
 export default function Home() {
-
-
-  return(
+  return (
     <main>
       <h1>CPRG306 Assignments</h1>
       <ul>
-        <li><Link href="./week-2/page.js"></Link>Week 2 Assignment</li>
+        <li>
+          <Link href="./week-2/">Week 2 Assignment</Link>
+        </li>
       </ul>
     </main>
   );
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return(
+        <div>
+            <h1>Yusif Najafli</h1>
+            <Link href="https://github.com/ynajafli/cprg306-assignments">GitHub Repository</Link>
+        </div>
+    );
+}
